f160953e649edbdd3bb9020bd7958549acecd372
peterhinners <peterhinners@gmail.com>
2016-02-24 14:57:34 -0500

    first text overlay

--- application.js
+++ application.js
@@ -1,5 +1,74 @@
 var map;
 function initMap() {
+
+
+  function TxtOverlay(pos, txt, cls, map) {
+
+      // Now initialize all properties.
+      this.pos = pos;
+      this.txt_ = txt;
+      this.cls_ = cls;
+      this.map_ = map;
+
+      // We define a property to hold the image's
+      // div. We'll actually create this div
+      // upon receipt of the add() method so we'll
+      // leave it null for now.
+      this.div_ = null;
+
+      // Explicitly call setMap() on this overlay
+      this.setMap(map);
+    }
+
+    TxtOverlay.prototype = new google.maps.OverlayView();
+
+
+
+    TxtOverlay.prototype.onAdd = function() {
+
+      // Note: an overlay's receipt of onAdd() indicates that
+      // the map's panes are now available for attaching
+      // the overlay to the map via the DOM.
+
+      // Create the DIV and set some basic attributes.
+      var div = document.createElement('DIV');
+      div.className = this.cls_;
+
+      div.innerHTML = this.txt_;
+
+      // Set the overlay's div_ property to this DIV
+      this.div_ = div;
+      var overlayProjection = this.getProjection();
+      var position = overlayProjection.fromLatLngToDivPixel(this.pos);
+      div.style.left = position.x + 'px';
+      div.style.top = position.y + 'px';
+      // We add an overlay to a map via one of the map's panes.
+
+      var panes = this.getPanes();
+      panes.floatPane.appendChild(div);
+    }
+    TxtOverlay.prototype.draw = function() {
+
+
+        var overlayProjection = this.getProjection();
+
+        // Retrieve the southwest and northeast coordinates of this overlay
+        // in latlngs and convert them to pixels coordinates.
+        // We'll use these coordinates to resize the DIV.
+        var position = overlayProjection.fromLatLngToDivPixel(this.pos);
+
+
+        var div = this.div_;
+        div.style.left = position.x + 'px';
+        div.style.top = position.y + 'px';
+
+
+
+      }
+
+
+
+
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 36.55547456, lng: -95.664999},
     zoom: 5
@@ -89,13 +158,58 @@ var chicago = new google.maps.LatLng(41.853895, -87.618449);
 var inBetween = google.maps.geometry.spherical.interpolate(nyc, chicago, 0.5);
 
 
-var markerTest = new google.maps.Marker({
+var marker = new google.maps.Marker({
   position: inBetween,
   map: map,
-  visible: false,
+  visible: false
 });
 
-var myLabel = new Label();
+customTxt = "<div>Blah blah</div>";
+txt = new TxtOverlay(inBetween, customTxt, "customBox", map);
+
+
+
+// var markerTest = new google.maps.Marker({
+//   position: inBetween,
+//   map: map,
+//   visible: true,
+//   icon: {},
+//   label: {
+//     text: 'Yo',
+//     color: 'purple'
+//   }
+// });
+
+// labelMarker = new google.maps.Marker({
+//         position: inBetween,
+//         map: map,
+//         visible: false
+//     });
+
+// var myLabel = new Label();
+
+//   myLabel.bindTo('position', labelMarker, 'position');
+//         myLabel.set("hey mom");
+//           myLabel.setMap(map);
+
+
+
+
+
+ // var mapLabel = new Label({
+ //        text: 'Test',
+ //        position: inBetween,
+ //        map: map,
+ //        fontSize: 35,
+ //        align: 'right'
+ //    });
+
+
+
+
+
+
+
 
 
 
@@ -120,3 +234,5 @@ var myLabel = new Label();
 
 
 }
+
+// google.maps.event.addDomListener(window, 'load', initMap);
